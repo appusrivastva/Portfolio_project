d5d87a8b280a63f3062d6928df3b1f1720f337a9
Apoorva <appumca2022@gmail.com>
2024-07-28 16:58:44 +0530

    gitignore change commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+const mail=process.env.MAIL || "abc@gmail.com"
+const pass=process.env.PASS || "xxxxxxxx";
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.post('/contact', (req, res) => {
+    const { name, email, message } = req.body;
+
+    // Set up Nodemailer transporter
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: mail,
+            pass:pass
+        }
+    });
+
+    const mailOptions = {
+        from: email,
+        to: 'appumca2022@gmail.com',
+        subject: `Contact form submission from ${name}`,
+        text: message
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return res.status(500).send(error.toString());
+        }
+        res.status(200).send('Message sent successfully');
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
