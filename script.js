21747e29df2166d5489ecd2fa73f947d483b2953
Apoorva srivastva <140163599+appusrivastva@users.noreply.github.com>
2024-07-30 20:47:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,32 +19,11 @@ let typingEffect=new Typed("#text",{
         document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
     }
 
-const form = document.getElementById('contactForm');
-  const messageDiv = document.getElementById('message');
+const form = document.getElementById('contact');
+
 
   form.addEventListener('submit', (event) => {
     event.preventDefault(); // फॉर्म को डिफ़ॉल्ट रूप से सबमिट होने से रोकता है
 
-    // फॉर्म डेटा को एक ऑब्जेक्ट में संग्रहित करें
-    const formData = new FormData(form);
 
-    // AJAX अनुरोध भेजें
-    fetch('https://formspree.io/f/mqazpbjp', {
-      method: 'POST',
-      body: formData
-    })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.text();
-    })
-    .then(data   
- => {
-      messageDiv.textContent = 'Your message has been sent!';
-    })
-    .catch(error => {
-      console.error('There has been a problem with your fetch operation:', error);
-      messageDiv.textContent = 'An error occurred. Please try again later.';
-    });
   });
